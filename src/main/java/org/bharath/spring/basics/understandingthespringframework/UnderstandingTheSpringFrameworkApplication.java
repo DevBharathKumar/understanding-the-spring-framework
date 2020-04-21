8e4ce07d577a3a1a988ee9ded1ea62562cebd1cf
Bharath Kumar <bharathk494@gmail.com>
2020-04-21 18:15:49 +0530

    Initial commit

---
+++ src/main/java/org/bharath/spring/basics/understandingthespringframework/UnderstandingTheSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package org.bharath.spring.basics.understandingthespringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderstandingTheSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderstandingTheSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/bharath/spring/basics/understandingthespringframework/UnderstandingTheSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.spring.basics.understandingthespringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingTheSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
