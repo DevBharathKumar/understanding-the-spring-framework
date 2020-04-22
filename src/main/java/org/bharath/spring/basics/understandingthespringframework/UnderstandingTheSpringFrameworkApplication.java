96582a616fa007e8559c741b3736bbd89122c69b
Bharath Kumar <bharathk494@gmail.com>
2020-04-22 21:03:31 +0530

    Adding logger into the application

--- src/main/java/org/bharath/spring/basics/understandingthespringframework/UnderstandingTheSpringFrameworkApplication.java
+++ src/main/java/org/bharath/spring/basics/understandingthespringframework/UnderstandingTheSpringFrameworkApplication.java
@@ -3,6 +3,8 @@ package org.bharath.spring.basics.understandingthespringframework;
 import org.bharath.spring.basics.understandingthespringframework.basic.BinarySearchImpl;
 import org.bharath.spring.basics.understandingthespringframework.basic.BubbleSortAlgorithm;
 import org.bharath.spring.basics.understandingthespringframework.basic.QuickSortAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,20 +12,23 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class UnderstandingTheSpringFrameworkApplication {
 
+	//Adding logger into the application 
+	private static Logger logger = LoggerFactory.getLogger(UnderstandingTheSpringFrameworkApplication.class);
+	
 	public static void main(String[] args) {
 		ApplicationContext applicationContext =  SpringApplication.run(UnderstandingTheSpringFrameworkApplication.class, args);
 		
 		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
 		
-		System.out.println("# Searched output : "+binarySearchImpl.binarySearch(new int[] {1, 0,6},3));
+		logger.info("{}",binarySearchImpl.binarySearch(new int[] {1, 0,6},3));
 		
 		//Understanding the singleton and prototype scope
 		//Singleton
-		System.out.println("#1 Singleton : "+applicationContext.getBean(BubbleSortAlgorithm.class));
-		System.out.println("#2 Singleton : "+applicationContext.getBean(BubbleSortAlgorithm.class));
+		logger.info("{}",applicationContext.getBean(BubbleSortAlgorithm.class));
+		logger.info("{}",applicationContext.getBean(BubbleSortAlgorithm.class));
 		//Prototype
-		System.out.println("#1 Prototype : "+applicationContext.getBean(QuickSortAlgorithm.class));
-		System.out.println("#2 Prototype : "+applicationContext.getBean(QuickSortAlgorithm.class));
+		logger.info("{}",applicationContext.getBean(QuickSortAlgorithm.class));
+		logger.info("{}",applicationContext.getBean(QuickSortAlgorithm.class));
 		
 	}
 
