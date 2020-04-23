d7ae9a5729c4fc3bd9956162579bfd4d15cb0cf2
Bharath Kumar <bharathk494@gmail.com>
2020-04-23 16:24:45 +0530

    Complex scope scenario - Solution

--- src/main/java/org/bharath/spring/basics/understandingthespringframework/scope/JdbcConnection.java
+++ src/main/java/org/bharath/spring/basics/understandingthespringframework/scope/JdbcConnection.java
@@ -2,9 +2,11 @@ package org.bharath.spring.basics.understandingthespringframework.scope;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE,
+		proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Component
 public class JdbcConnection {
 	
