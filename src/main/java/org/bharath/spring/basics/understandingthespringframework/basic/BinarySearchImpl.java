b75a92d58768d281255d9f667a307090fdd95b4d
Bharath Kumar <bharathk494@gmail.com>
2020-04-24 17:52:10 +0530

    Understanding @PostConstruct and @PreDestroy

--- src/main/java/org/bharath/spring/basics/understandingthespringframework/basic/BinarySearchImpl.java
+++ src/main/java/org/bharath/spring/basics/understandingthespringframework/basic/BinarySearchImpl.java
@@ -1,5 +1,10 @@
 package org.bharath.spring.basics.understandingthespringframework.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -8,6 +13,8 @@ import org.springframework.stereotype.Component;
 public class BinarySearchImpl 
 {
 
+	Logger logger = LoggerFactory.getLogger(BinarySearchImpl.class);
+	
 	private SortAlgorithm sortAlgorithm;
 
 	@Autowired
@@ -28,5 +35,17 @@ public class BinarySearchImpl
 		// Search the array
 
 		return numberToBeFound;
+	}
+	
+	@PostConstruct
+	public void postConstruct() //We can give any name to the method by we should use @PostConstruct annotation
+	{
+		logger.info("Inside the post construct method ");
+	}
+	
+	@PreDestroy
+	public void preDestroy()
+	{
+		logger.info("Inside the pre destroy method ");
 	}
 }
